cbcd035658b9c9017e74aea328ac450cb55f00d4
jatoni <jat_antonio_torres.juan@outlook.com>
2022-10-04 01:44:33 -0500

    Se hace un buscador de imagenes con un api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Formik, Form, Field } from 'formik';
+import './header.css';
+import './content.css';
+import './article.css';
 
-function App() {
+const App = () => {
+  const [photos, setPhotos] = useState([])
+  const open = url => window.open(url)
+  console.log({ photos })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <header>
+          <Formik
+            initialValues={{ search: '' }}
+            onSubmit={async values => {
+              const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
+                headers: {
+                  'Authorization': "Client-ID mwBIYDml9XjmLphOUHvZ6karq8x_X94bpxWJaGOezA0"
+                }
+              })
+              const data = await response.json()
+              // llamar a api de unplash
+              setPhotos(data.results);
+            }}
+          >
+            <Form>
+              <Field name="search" />
+            </Form>
+          </Formik>
+        </header>
+        <div className="container">
+          <div className="center">
+            {photos.map(photo => 
+              <article key={photo.id} onClick={() => open(photo.links.html)} >
+                <img src={photo.urls.regular} alt={photo.alt_descripion} />
+                <p>{[photo.description, photo.alt_descripion].join(' - ')}</p>
+              </article>
+            )}
+          </div>
+        </div>
+      </div>
   );
 }
 
